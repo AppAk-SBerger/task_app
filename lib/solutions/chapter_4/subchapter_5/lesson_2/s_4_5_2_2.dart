40d5c960a16698bf4cc82d62e8edae6f8e31a3bc
SBerger_DEV <teebeutel.de@gmail.com>
2025-01-06 13:53:33 +0100

    solved 4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,22 @@ class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Column(
+        children: [
+          Text("Hello Sebastian"),
+          ElevatedButton(onPressed: null, child: Text("Klick mich")),
+        ],
+      ),
+    );
   }
 }
