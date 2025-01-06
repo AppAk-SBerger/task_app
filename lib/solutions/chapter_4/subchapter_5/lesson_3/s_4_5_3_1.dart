afd900013866aa34a02c331b140845243241121b
SBerger_DEV <teebeutel.de@gmail.com>
2025-01-06 14:02:48 +0100

    solved 4.5.3.1

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,26 @@ class S4531 extends StatelessWidget {
   const S4531({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Column(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blueGrey,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.cyan,
+          ),
+        ],
+      ),
+    );
   }
 }
